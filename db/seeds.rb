ddebafa25565cf8a0b03d1636d835b0bc066c733
weyewe <w.yunnal@gmail.com>
2013-02-27 14:16:45 +0700

    added changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,23 +15,16 @@ first_role = Role.first
  
 
 
-company = Company.create(:name => "Super metal", :address => "Tanggerang", :phone => "209834290840932")
+company = Company.create(:name => "Dikarunia", :address => "Tanggerang", :phone => "209834290840932")
 admin = User.create_main_user(   :email => "admin@gmail.com" ,:password => "willy1234", :password_confirmation => "willy1234") 
 
 admin.set_as_main_user
-
-vendor = Vendor.create({
-    :name =>"Monkey Crazy", 
-    :contact_person =>"", 
-    :phone =>"", 
-    :mobile =>"", 
-    :bbm_pin =>"", 
-    :email =>"", 
-    :address =>""})
+ 
     
+employee = Employee.create :name => "Joni"
 # create vendor  => OK 
 Vendor.create({
-    :name =>"Monkey Crazy", 
+    :name =>"Supplier 1", 
     :contact_person =>"", 
     :phone =>"", 
     :mobile =>"", 
@@ -43,37 +36,37 @@ Vendor.create({
 base_item_category =  ItemCategory.create_base_object( admin, :name => "Base Item" ) 
 
 # create item  
-
-
-test_item  = Item.create_by_employee(  admin,  {
-  :name => "Test Item",
-  :supplier_code => "BEL324234",
-  :customer_code => 'CCCL222',
-  :item_category_id => base_item_category.id 
-})
-
- 
-# create stock migration 
-test_item_migration =  StockMigration.create_by_employee(admin, {
-  :item_id => test_item.id,
-  :quantity => 200
-})  
-
-# create stock adjustment 
-# StockAdjustment.create_item_adjustment(admin , test_item , 150 ) 
-
-# create purchase  + purchase entry 
-purchase_order = PurchaseOrder.create_by_employee( admin, {
-  :vendor_id => vendor.id 
-} )
-
-quantity_purchased  =  6
-purchase_order_entry = PurchaseOrderEntry.create_by_employee( admin, purchase_order, {
-  :item_id => test_item.id ,
-  :quantity => quantity_purchased
-} )
-
-purchase_order.confirm( admin )
+# 
+# 
+# test_item  = Item.create_by_employee(  admin,  {
+#   :name => "Test Item",
+#   :supplier_code => "BEL324234",
+#   :customer_code => 'CCCL222',
+#   :item_category_id => base_item_category.id 
+# })
+# 
+#  
+# # create stock migration 
+# test_item_migration =  StockMigration.create_by_employee(admin, {
+#   :item_id => test_item.id,
+#   :quantity => 200
+# })  
+# 
+# # create stock adjustment 
+# # StockAdjustment.create_item_adjustment(admin , test_item , 150 ) 
+# 
+# # create purchase  + purchase entry 
+# purchase_order = PurchaseOrder.create_by_employee( admin, {
+#   :vendor_id => vendor.id 
+# } )
+# 
+# quantity_purchased  =  6
+# purchase_order_entry = PurchaseOrderEntry.create_by_employee( admin, purchase_order, {
+#   :item_id => test_item.id ,
+#   :quantity => quantity_purchased
+# } )
+# 
+# purchase_order.confirm( admin )
 
 # create purchase receive + purchase receive entry 
 # create delivery  + delivery entry 
